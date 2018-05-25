423e4285054be612172acdb0acf7a260cf68d008
Christian Clark <christianclark687@gmail.com>
2018-05-24 22:58:08 -0500

    Abilty to change the dimensions of the grid
    User can now change the x and y dimensions of the grid by changing the values of the form inputs.
    Any values that are above 100 or not a number will default to 12.

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+let reset = document.getElementById('reset');
+let x = document.getElementById('x-dimension');
+let y = document.getElementById('y-dimension');
+
+x.addEventListener('change', changeX);
+y.addEventListener('change', changeY);
+
+function clear() {
+  let cells = document.querySelectorAll('.cell');
+  let area = document.querySelector('.drawing-area');
+  cells.forEach(function(cell) {
+    area.removeChild(cell);
+  })
+}
 
 function draw(grid) {
-  for (i = 0; i < Math.pow(grid, 2); i++) {
-    let cell = document.createElement('div');
-    cell.className = 'cell ' + classes[0];
-    let cellSize = (512 / grid);
+  for (x = 0; x < xDim; x++) {
+    for (y = 0; y < yDim; y++) {
+      let cell = document.createElement('div');
+      cell.className = 'cell';
+      let width = (512 / xDim);
+      let height = (512 / yDim);
 
-    cell.style.width = cellSize + 'px';
-    cell.style.height = cellSize + 'px';
-    cell.style.opacity = 0;
-    document.querySelector('.drawing-area').appendChild(cell);
-    cell.addEventListener('mouseover', darken);
+      cell.style.width = width + 'px';
+      cell.style.height = height + 'px';
+      cell.style.opacity = 0;
+      document.querySelector('.drawing-area').appendChild(cell);
+      cell.addEventListener('mouseover', darken);
+    };
   };
 }
 
@@ -17,4 +34,29 @@ function darken(event) {
   event.target.style.opacity = 1;
 }
 
-draw(12);
+function changeX(event) {
+  xDim = event.target.value;
+  if (isNaN(xDim) || xDim > 100) {
+    xDim = 12;
+    event.target.value = 12;
+  };
+  clear();
+  draw(xDim, yDim);
+};
+
+function changeY(event) {
+  yDim = event.target.value;
+  if (isNaN(yDim) || yDim > 100) {
+    yDim = 12;
+    event.target.value = 12;
+  };
+  clear();
+  draw(xDim, yDim);
+};
+
+
+let xDim = document.getElementById('x-dimension').value;
+let yDim = document.getElementById('y-dimension').value;
+console.log(xDim);
+console.log(yDim);
+draw(xDim, yDim);
