79ebb6dd3f47cc2f3756e1eafdfafafa18a243d0
Christian Clark <christianclark687@gmail.com>
2018-05-24 20:39:24 -0500

    Can darken squares by rolling over them
    Squares change color when rolled over by the mouse.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-
 function draw(grid) {
   for (i = 0; i < Math.pow(grid, 2); i++) {
-    var cell = document.createElement('div');
-    cell.className = 'cell';
-    var cellSize = (512 / grid);
+    let cell = document.createElement('div');
+    cell.className = 'cell ' + classes[0];
+    let cellSize = (512 / grid);
 
     cell.style.width = cellSize + 'px';
     cell.style.height = cellSize + 'px';
-    cell.addEventListener('mouseover', darken);
+    cell.style.opacity = 0;
     document.querySelector('.drawing-area').appendChild(cell);
-  }
+    cell.addEventListener('mouseover', darken);
+  };
 }
 
 function darken(event) {
-  
+  event.target.style.opacity = 1;
 }
 
 draw(12);
